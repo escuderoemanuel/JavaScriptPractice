662e59eeafb537901e6481ee68f39a5b346c069f
Emanuel Escudero <escudero.mdz@gmail.com>
2023-09-02 19:47:50 -0300

    First Practices

---
+++ practice.js
@@ -0,0 +1,15 @@
+// Ctrl + Alt + N => Visualizar la salida de consola
+
+// The goal of this challenge is to create a JavaScript function that takes a string as an argument and returns the string in lower case.
+function lowerCase(text) {
+  return text.toLowerCase();
+}
+console.log(lowerCase('CONVERT THIS TEXT IN LOWER CASE'));
+
+// The goal of this challenge is to create a JavaScript function that takes a string as an argument and returns the string in reverse order.
+function reverOrder(text) {
+  return text.split('').reverse().join('');
+}
+console.log(reverOrder('This text is reverse!'));
+
+//
